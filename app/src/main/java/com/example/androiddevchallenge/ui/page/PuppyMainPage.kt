ff75df27bef895294b1b41bfbde7df4d1cd5a72a
haha <haha@haha>
2021-02-26 10:12:38 +0800

    code formatting

--- app/src/main/java/com/example/androiddevchallenge/ui/page/PuppyMainPage.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/page/PuppyMainPage.kt
@@ -1,21 +1,18 @@
 /*
+ * Copyright 2021 The Android Open Source Project
  *
- *  * Copyright 2021 The Android Open Source Project
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  *
- *  *     https://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package com.example.androiddevchallenge.ui.page
 
 import androidx.compose.material.Scaffold
@@ -24,12 +21,12 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.livedata.observeAsState
-import com.example.androiddevchallenge.model.Puppy
-import com.example.androiddevchallenge.viewmodel.PuppyViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.navigate
+import com.example.androiddevchallenge.model.Puppy
 import com.example.androiddevchallenge.ui.PuppyList
+import com.example.androiddevchallenge.viewmodel.PuppyViewModel
 
 @Composable
 fun PuppyMainPage(
